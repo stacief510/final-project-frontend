69147330e0385f8ce2956161ee6c989ba10b3c80
stacief510 <staciemfraser@gmail.com>
2018-05-28 17:02:18 -0700

    added new links to the header/navbar

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ class Header extends Component{
             <div>
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                            <a className="navbar-brand roast" href="#">ROAST</a>
+                            <a className="navbar-brand roast" href="/roast">ROAST</a>
                             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                             </button>
@@ -17,7 +17,7 @@ class Header extends Component{
                                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                                 </li>
                                 <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                                <a className="nav-link" href="/roast/find">Find Coffee</a>
                                 </li>
                                 <li className="nav-item">
                                 <a className="nav-link disabled" href="#">Disabled</a>
